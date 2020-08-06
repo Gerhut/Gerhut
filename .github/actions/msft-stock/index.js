22d10c1d0896bb4877d96ec67b991b2dcf3f2e05
George Cheng <Gerhut@GMail.com>
2020-08-06 14:56:31 +0800

    Local actions

---
+++ .github/actions/msft-stock/index.js
@@ -0,0 +1,39 @@
+const core = require('@actions/core')
+const asciichart = require('asciichart')
+const fetch = require('node-fetch')
+
+async function main (alphaVantageAPIKey) {
+  const url = new URL('/query', 'https://www.alphavantage.co/')
+  url.searchParams.set('function', 'TIME_SERIES_DAILY')
+  url.searchParams.set('symbol', 'MSFT')
+  url.searchParams.set('apikey', alphaVantageAPIKey)
+
+  const response = await fetch(url.toString())
+  if (!response.ok) {
+    throw Error(`HTTP ${response.status}`)
+  }
+
+  const data = await response.json()
+  const meta = data['Meta Data']
+  const series = data['Time Series (Daily)']
+  const prices = Object.keys(series).sort().map(function (time) {
+    const price = Number(series[time]['4. close'])
+    return Number.isFinite(price) ? price : undefined
+  })
+
+  const chart = asciichart.plot(prices, { height: 20 })
+  return `${chart}\n\n> Last Refreshed: ${meta['3. Last Refreshed']}`
+}
+
+if (require.main === module) {
+  main(process.env.ALPHA_VANTAGE_API_KEY).then(function (chart) {
+    console.log(chart)
+    core.setOutput('chart', chart)
+    process.exit(0)
+  }, function (error) {
+    console.error(error)
+    process.exit(1)
+  })
+} else {
+  module.exports = main
+}
